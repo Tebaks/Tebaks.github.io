e8d03c917407fae8ee50c58e873909294751e93f
Kenan Abbak <kenanabbak@hotmail.com>
2021-08-30 11:04:44 +0300

    Updates

---
+++ precache-manifest.347ac23f062b8e3271406f8a92ab402b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce03c20bd96fe2bba4eba0bd412e59d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bb9651c92157d324bbe",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6c437facc0985ffd82da",
+    "url": "./static/css/main.10ff02de.chunk.css"
+  },
+  {
+    "revision": "7bb9651c92157d324bbe",
+    "url": "./static/js/2.02221423.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.02221423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c437facc0985ffd82da",
+    "url": "./static/js/main.bbb38fb7.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
